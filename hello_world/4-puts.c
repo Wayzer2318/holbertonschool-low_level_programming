1e7c7b77bb221772fe786b740f2fca602d8bdf00
Wayzer2318 <arezki.ait.kettout@gmail.com>
2022-09-30 05:49:38 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- *my first program 
-*/
 
-int main ()
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

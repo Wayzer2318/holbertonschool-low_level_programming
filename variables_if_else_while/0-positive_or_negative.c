dcc880d94ec09fa9f06a7211f70231553fd81d93
Wayzer2318 <arezki.ait.kettout@gmail.com>
2022-10-03 01:36:38 -0700

    positive and negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n >= 0)
-	{
-		printf("%d is a positive number \n", n);
+	
+		printf("%d is a positive \n", n);
 	else
-		printf("%d is a negative number \n", n);
-	}
+		printf("%d is a negative \n", n);
+	
 	return (0);
 }

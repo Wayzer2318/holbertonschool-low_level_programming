a1756b808c0710afe34a9bb5c6c7792184c0ac39
Wayzer2318 <arezki.ait.kettout@gmail.com>
2022-10-03 05:09:38 -0700

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar("%c", ch '\n');
+		ch++;
+
+	return (0);
+}

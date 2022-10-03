f3692b8e4711e44dd5fa88f318ad7a8c5a3f0b6f
Wayzer2318 <arezki.ait.kettout@gmail.com>
2022-10-03 08:01:51 -0700

    print number

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}

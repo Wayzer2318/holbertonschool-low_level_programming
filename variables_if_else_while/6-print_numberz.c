ccd8012b45cc3e0dd79f287c694fe495c22b7cc9
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-03 17:26:02 +0200

    print numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	int i
+
+	for (i=0, i<=9, i++)
+		putchar('o'+i=);
+	return (0);
+}

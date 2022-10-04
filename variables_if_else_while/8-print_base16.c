4b48762fd12fc47a32e504d6d68b1f5d2c3c3e3d
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-04 16:21:35 +0200

    print hex

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for(num = 0; num <= 9;num++)
+	{
+		putchar(num + '0');
+	}
+	for(ch ='a'; ch <= 'z'; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}

4b1d9fcbe0409cd87feb32f7f0e26d34b0396e88
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-04 13:25:12 +0200

    print reversed alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}

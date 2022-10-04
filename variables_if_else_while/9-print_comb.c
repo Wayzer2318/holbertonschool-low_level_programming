3dc78d6714256590036a0a19238f6896e6aaee90
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-04 16:59:30 +0200

    print numbers fllowed by comma

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ *
+ * Return: exit status code
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

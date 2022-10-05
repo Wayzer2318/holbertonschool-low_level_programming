480d31009d26a2a9c1d8e9fd00e02fe94eefdfbe
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-05 13:36:10 +0200

    function that print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: fonction
+ * Return: 0
+ * print_alphabet - function
+ * description: fonction that print the alphabet
+ */
+
+int main(void)
+{
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}

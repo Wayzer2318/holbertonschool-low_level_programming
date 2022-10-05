42b7af2b4fd380d63fe339b3d2869701a1f6ca8f
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-05 14:45:59 +0200

    function that print alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet_x10 - function
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+}

a9b16656a843f56c4061f506437b0d20d44a5d23
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-05 13:47:52 +0200

    function that print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  *print_alphabet - function
  */
 

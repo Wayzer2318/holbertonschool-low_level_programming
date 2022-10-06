cfa9822d440dc4e367cdd5d9582e1492b3e41f52
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-06 14:16:07 +0200

    print sign fonction

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - check if the charactere is alphabet.
+ * @c: the caractere to check.
+ * Return: return 1 or 0
+ */
+
+int print_sign(int n)
+{
+	if (c < 0)
+	{
+		print("-");
+		return (-1);
+	}
+	else if (c > 0 )
+	{
+		printf("+");
+		return (1);
+	}
+	else
+	{
+		print("0");
+		return (0);
+	}
+}

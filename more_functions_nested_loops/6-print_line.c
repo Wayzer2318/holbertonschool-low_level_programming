1461c5ffba65f086759bcae1db55ea42d2525322
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-07 14:00:15 +0200

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "main.h"
-
-/**
- * print_line - print lines
- *
- * Return: Always 0.
- */
 
 void print_line(int n)
 {
-	int i;
+	int x = 0;
 
-	for (i = 0; i <= n; i++)
+	while ( x < n)
 	{
 		if (n > 0)
-			_putchar('_');
-		else
-			_putchar('\n');
+			{
+				_putchar (95);
+				x++;
+			}
+			else
+			{
+				_putchar ('\n');
+			}
 	}
-}
+	_putchar ('\n');
+}}

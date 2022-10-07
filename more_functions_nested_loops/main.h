061e432787e1eb3b1500a0bdb3245dbfab912ff0
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-07 12:53:36 +0200

    task4

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);

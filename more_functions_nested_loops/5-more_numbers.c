3ab79fbcd7cef0a6dfddf3d19a645f6aa77a6682
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-07 13:14:32 +0200

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	char a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+				_putchar ((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar ('\n');
+	}
+}

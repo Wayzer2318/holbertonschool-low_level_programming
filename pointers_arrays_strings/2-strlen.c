0f8e02c1a5ea3729a85dfbe0ae25ec7ceb84bf0b
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-10 17:44:24 +0200

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - point to an int
+ * @s: string
+ * Return: length of string.
+ */
+int _strlen(char *s);
+{
+	int i
+
+	for (i = 0; i[s] != '\0')
+		i++;
+	return (i);
+}

547bcfeba2f4dd7c107493516d06547e393a8341
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-11 12:45:16 +0200

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - function that prints the string
+ * @str: variable
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	len = _strlen(*s);
+	for (i = 0; i < (len / 2); i++)
+	{
+		*s[len] = *s[i];
+	}
+}

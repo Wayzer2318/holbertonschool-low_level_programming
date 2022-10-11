f58565234b618bb312b7af974414e33423e6841a
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-11 14:32:50 +0200

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * print_rev - function that prints the string
- * _strlen - function that count 
- *@str: variable
+ * _strlen - function that count
+ * @len: lenth
  * Return: 0
  */
 int _strlen(char *s)
@@ -23,7 +23,7 @@ void print_rev(char *s)
 	len = _strlen(s);
 	for (i = len - 1; i >= 0; i--)
 	{
-		
+
 		putchar(s[i]);
 	}
 	putchar('\n');

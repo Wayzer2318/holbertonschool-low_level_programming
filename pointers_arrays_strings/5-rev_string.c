434fb96d864fcf545c56f538adb99f08f7bf802b
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-11 16:13:48 +0200

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * void rev_string(char *s) - function that reverse the string
+ * @s: variable
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}

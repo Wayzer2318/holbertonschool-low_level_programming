cd0f85a5faef235feaeecef86eb97c96b1e910d3
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-11 12:52:31 +0200

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,19 @@
 #include "main.h"
 /**
  * print_rev - function that prints the string
- * @str: variable
+ * _strlen - function that count 
+ *@str: variable
  * Return: 0
  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 void print_rev(char *s)
 {
 	int i;

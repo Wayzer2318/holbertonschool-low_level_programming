70c6bfef70206a7ca166616270a03485e8ef761c
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 20:50:10 +0200

    task8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[n]);
+		printf("%d", a[i]);
 		if ( i < n -1)
-			printf(", ");
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
 }

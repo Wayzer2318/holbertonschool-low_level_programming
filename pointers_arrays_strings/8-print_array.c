95b2a9c8a53acfe1c65bf8b40ef754db2ae1558b
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 20:39:28 +0200

    task7

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - function that prints n elements of an array of integers
+ * @a: variable
+ * @n: variable
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	
+	printf("\n");
+}

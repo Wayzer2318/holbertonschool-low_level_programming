36003691fd78b6bb6c4ba57a366f1b7abe0f1053
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 19:35:40 +0200

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0;str[i] =! '\0'; i++)
+	for (i = 0;(str[i] =! '\0'); i++)
 	{
-		if ((str[i] % 2) == 0)
+		if ((i % 2) == 0)
 		{
-			printf("%c", str[i]);
+			putchar(str[i]);
 		}
 		else
 		{

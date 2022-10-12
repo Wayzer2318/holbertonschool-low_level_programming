3642bd30723cb335e9fc207889631af95dba32dd
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 21:34:47 +0200

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * _strcpy - function that copies the string
+ * @dest: variable
+ * @src: variable
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = &src;
+	return (dest);
+}

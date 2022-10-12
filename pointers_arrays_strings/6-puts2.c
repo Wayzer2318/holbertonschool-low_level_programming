b345db18cdb9a728b612ad3c98f1f0f29e695ba9
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 19:20:40 +0200

    task5

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - function  that prints every other character of a string
+ * @str: variable
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] =! '\0')
+	{
+		if (s[i] % 2 == '0')
+		{
+			printf("%c", s[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+}

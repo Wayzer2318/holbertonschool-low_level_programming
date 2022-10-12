78685a506091c9f83fb53f9fec12e3a716292347
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-12 21:55:17 +0200

    task9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i < '\0'; i++)
+	for (i = 0; i != '\0'; i++)
 	{
 		dest[i] = src[i];
-		putchar(dest[i]);
 	}
-	return (0);
+	dust[i] = '\0'
+	return (dest);
 }

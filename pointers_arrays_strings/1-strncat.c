4621bd8bb4c5ed5090949560a1bdfad084747771
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-13 11:02:35 +0200

    task0

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenate two string
+ * @src: variable
+ * @dest: variable
+ * @n: variable
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        int i = 0;
+        int x = 0;
+
+        for (i = 0; dest[i] != '\0'; i++)
+        {
+        }
+
+        for (x = 0; src[x] != '\0'; x++)
+        {
+                dest[i] = src[x];
+                i++;
+        }
+        return (dest);	
+}

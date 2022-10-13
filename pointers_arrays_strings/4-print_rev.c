f52ea88224c4e153b2b42b629e8f0efb9feccbc1
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-13 12:15:41 +0200

    task4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_rev - function that prints the string in reverse
- * @s: variable
+ * reverse_array - Function that reverse an array
+ * @a: array
+ * @n: bytes
  * Return: 0
  */
-void print_rev(char *s)
+void reverse_array(int *a, int n)
 {
-	int i = 0;
+	int i;
+	int x;
+	int temp[1000];
 
-	while (s[i] != '\0')
+	x = 0;
+	while (x < n)
 	{
+		temp[x] = a[x];
+		x++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		a[i] = temp[x - 1];
+		x--;
 		i++;
 	}
-	i--;
-	while (i >= 0)
-	{
-		putchar(s[i]);
-		i--;
-	}
-	putchar('\n');
 }

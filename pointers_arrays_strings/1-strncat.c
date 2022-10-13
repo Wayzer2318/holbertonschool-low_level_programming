085317465f243f112ff5f04753d94f253f3acc3f
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-13 11:14:42 +0200

    task0

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
         {
         }
 
-        for (x = 0; src[x] != '\0'; x++)
+        for (x = 0; src[x] != n; x++)
         {
                 dest[i] = src[x];
                 i++;

39f911934805ef665fa971a116697458cb38d2e2
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-17 12:17:06 +0200

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - recursion
+ * @s: variable
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if(s[i] != '/0')
+	{
+		putchar(s[i]);
+		_puts_recursion(i + 1);
+	}
+}

523a85dbf8b03d21f450668755b93e6bf4cbc297
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-17 15:18:45 +0200

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _print_rev_recrsion - print a string in reverse
+ * _print_rev_recursion - print a string in reverse
  * @s: variable
  */
 void _print_rev_recursion(char *s)
 {
-	if(!*s)
+	if (!*s)
 	return;
 	{
 		_print_rev_recursion(s + 1);

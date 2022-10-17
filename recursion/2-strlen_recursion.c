bf8ce3968e420b60afc5ff2890403df0617d8106
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-17 16:44:53 +0200

    task2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,10 +3,11 @@
 /**
  * _strlen_recursion - strlen recursion
  * @s: variable
+ * return: nothing
  */
 int _strlen_recursion(char *s)
 {
-	if(!*s)
+	if (!*s)
 		return 0;
 	return (1 + _strlen_recursion(s + 1));
 }

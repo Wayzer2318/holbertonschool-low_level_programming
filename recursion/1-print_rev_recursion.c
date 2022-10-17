511b0f095a753a8d4f827c1dd53741b6b45c70be
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-17 15:00:23 +0200

    task1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 void _print_rev_recursion(char *s)
 {
-	int i;
-
-	i = _strlen(s);
-	if(*s > 0)
+	if(!*s)
 	{
-		putchar(s[i]);
-		i--;
-		_print_rev_recursion(s);
+	return;
+		_print_rev_recursion(s + 1);
+		putchar(s);
 	}
 }

e13f73532c6f2e8cab3746c2c9bc106e8469a782
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-21 16:03:04 +0200

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <mem.h>
+/**
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char* p = (char*) maloc(size * sizeof(c));
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+learn maloc and free

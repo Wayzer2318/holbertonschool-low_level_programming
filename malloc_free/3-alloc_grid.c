8457a0e2d5e22ec24b2ed274546df686b26ddfc6
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-10-26 22:25:45 +0200

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,9 +11,23 @@ int **alloc_grid(int width, int height)
 	int i;
 	int **grid;
 
+	if (width <= 0 || height <= 0)
+		return ('\0');
+
 	grid = (int**) malloc(sizeof(int*) * height);
+
+	if (grid == NULL)
+		return ('\0');
 
 	for (i = 0; i < width; i++)
 		grid[i] = (int*) malloc(sizeof(int) * width);
+		if (arr[hgt_index] == NULL)
+		{
+			for (; hgt_index >= 0; hgt_index--)
+				free(arr[hgt_index]);
+
+			free(arr);
+			return ('\0');
+		}
 	return (grid);
 }

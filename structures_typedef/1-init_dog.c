798b5d159f5b42f8b7de4c324e9509797bff6028
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-01 15:18:59 +0100

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * init_dog - init a new dog
+ *
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strcpy(p->name, &name);
+	p->age = &age;
+	strcpy(p->owner, &owner);
+}

bf6766dbed6d19e456b522900dbc16ca9d79f2f0
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-01 14:59:51 +0100

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
-#include <stdio.h>
+/**
+ * struct dog - define a new struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 	char *name;

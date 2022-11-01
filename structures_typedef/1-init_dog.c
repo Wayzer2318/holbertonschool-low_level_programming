883a11ab3527dce6e0ad362c491cc063a56756fd
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-01 15:25:12 +0100

    task1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,15 +3,17 @@
 #include <string.h>
 /**
  * init_dog - init a new dog
- *
- *
+ * @d: new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
-		p->name = name;
-		p->age = age;
-		p->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }

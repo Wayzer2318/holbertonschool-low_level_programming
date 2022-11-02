7eed6ddfe8e84706f5990d3b1a8c0591ab33243e
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-02 18:04:03 +0100

    task4

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free a dog
+ * @d: dog to free
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+	
+}

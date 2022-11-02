f02c2e3d3c8956d9337e98d3170bf51c40d4dc3d
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-02 17:43:45 +0100

    task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned i;
+	unsigned j;
+
+	if (str == NULL)
+		return ('\0')
+	while(str[i])
+		i++;
+	arr = malloc(sizeof(char) * (i + 1));
+	if (arr == NULL)
+		return ('\0');
+	while (str[j])
+		arr[j] = str[j];
+		j++;
+	arr[j] = 0;
+	return (arr);
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *namecp;
+	char *ownercp;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return ('\0');
+	namecp = strdup(name);
+	if (namecp != name)
+	{
+		free(new);
+		return('\Ø');
+	}
+	ownercp = strdup(owner);
+	if (ownercp != owner)
+	{
+		free(namecp);
+		free(new);
+		return ('\0');
+	}
+
+	new->name = namecp;
+	new->age = age;
+	new.owner = ownercp;
+
+	return (new);
+}

e8a0ec320deaa2e1e82b34d5849f2bdde1277780
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-04 14:17:22 +0100

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#unclude "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int (*p)(int);
+
+	p = cmp
+
+	if (size <= 0)
+		return (-1);
+	if (!array || !cmp)
+		return (-1)
+
+	for (i = 0; i < size; i++)
+	{
+		if(p(array[i] && array[i] != 0))
+		break;
+		if (i == size - 1)
+			return(-1);
+	}
+	return (i);
+}

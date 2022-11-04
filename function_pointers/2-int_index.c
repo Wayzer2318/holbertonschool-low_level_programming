aceb0d56b0cfe47edda4d5f39b26eb345a740bc6
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-04 14:22:01 +0100

    task2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * int_index - Function that searches for an integer
+ * @array: variable that contains the array of integer
+ * @size: size of the elements in the array
+ * @cmp: pointer to the function that capare values
+ * Return: -1 or the first element for which the cmp
+ * function does not return 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -15,10 +22,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 {
-		if(p(array[i]) && array[i] != 0)
+		if (p(array[i]) && array[i] != 0)
 		break;
 		if (i == size - 1)
-			return(-1);
+			return (-1);
 	}
 	return (i);
 }

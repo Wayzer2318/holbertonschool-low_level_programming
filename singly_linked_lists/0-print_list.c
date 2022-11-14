421eff43b838eae6d2c5d812f07c95cf6782173c
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-14 18:55:25 +0100

    task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - function
+ * @h: node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n")
+		else
+			printf("[%i] ‰s", h->len, h->str);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}

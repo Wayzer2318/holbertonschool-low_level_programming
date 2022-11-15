fed83cd469e522d1725d80ed4c3076375f0025ed
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-15 22:44:35 +0100

    task 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strlen - length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * add_node_end - add a new node at the end
+ * @str: string to add
+ * @head: the node in the head, first node
+ * Return: a new node
+ */
+list_t *add_node_end(list_t **head, const char *str);
+{
+	list_t *result = malloc(sizeof(list_t));
+
+	if (result == NULL)
+		return ('\0');
+	result->len = strlen(str);
+	result->str = strdup(str);
+	*head->next = result;
+	*head = result;
+	return (result);
+}

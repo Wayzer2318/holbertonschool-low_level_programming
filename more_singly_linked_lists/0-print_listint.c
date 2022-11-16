645e10c2ba24cbbdcb7a657b551661094fa70dda
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-16 23:31:07 +0100

    task0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * print_listint - print a list
+ * @h: head of the list
+ * Return: number of int
  */
 size_t print_listint(const listint_t *h)
 {

0212f4bdc565b7e454308c6a4b933b132ad20cfa
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-17 01:14:02 +0100

    task1

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * listint_len - length of a list
+ * @h: the head of the list
+ * Return: lenght of the list
  */
 size_t listint_len(const listint_t *h)
 {

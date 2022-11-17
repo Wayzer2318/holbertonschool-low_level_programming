bf32f7fba29e2eb5a00d02ca59e42ff5df6ab63a
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-17 01:26:10 +0100

    task2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *node;
 
 	node = malloc(sizeof(listint_t));
-	if (node = NULL)
+	if (node == NULL)
 		return('\0');
 	node->n = n;
 	node->next = *head;

3740392b76655d316b080306a9c1b37b4a291ee7
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-22 19:12:48 +0100

    task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: double pointer address of node
+ * @n: node to add
+ * Return: address of the new element, or NULL if it failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;

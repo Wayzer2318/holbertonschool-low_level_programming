3cafe115fc325400567cf78b0890eedd0e7bf35c
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-22 17:42:34 +0100

    task 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return(-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	wr = write(fd, text_content, strlen(text_content));
+	close(fd);
+	if (wr == -1)
+		return(-1);
+
+	return (1);
+}

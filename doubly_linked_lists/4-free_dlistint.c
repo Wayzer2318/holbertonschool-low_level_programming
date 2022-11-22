5f0d42724063748631f1f51cec0a4377af7de013
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-22 19:59:17 +0100

    task 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * free_dlistint - Function that frees a dlistint_t list.
+ * @head: pointer to the first node
+ * Return: nothing
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;

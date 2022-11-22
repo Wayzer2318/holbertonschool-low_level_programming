ed447019a7168ba92d5c561d79c3bcbb530d8192
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-22 19:06:19 +0100

    task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,8 +4,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t node;
 
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+	{
+		free(node);
+		return ('\0');
+	}
 	node->n = n;
-	node_>prev = NULL;
-	node->next = *head;
+	node->prev = NULL;
+	if (*head == NULL)
+	{
+		node = *head;
+		node->next = NULL;
+	}
+	else
+	{
+		node->next = *head;
+		(*head)->prev = node;
+		*head = node;
+	}
 	return (node);
 }

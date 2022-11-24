f84e80a3ee5ecdda080349ad98cde8bcc13ce99c
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-11-24 15:21:41 +0100

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *tmpcount = *h, *tmp2;
+	dlistint_t *tmp = *h, *tmp2;
 	dlistint_t *new;
 	unsigned int counter = 0;
 

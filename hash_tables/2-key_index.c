c52e08f9d16c130a36ac3aa7a0b93fee28a1835e
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-12-10 00:42:35 +0100

    task2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,19 +1,12 @@
 #include "hash_tables.h"
-/**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
 
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+/**
+ * key_index - gives index of a key
+ * @key: the key
+ *@size: uli
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
 }

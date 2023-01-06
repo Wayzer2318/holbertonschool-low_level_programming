2b89f9252d0d1dd9ed70f609fb6b207854ca0bbd
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-06 23:17:11 +0100

    fixed bug for the search algo file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,8 +1,10 @@
-#define _SEARCH_ALGOS_H_
-#ifndef _SEARCH_ALGOS_H_
+#ifndef _SEARCH_ALGO
+#define _SEARCH_ALGO
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif

4c1d2e82ac630af296f0f67bb309f52375d6f191
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-06 17:31:04 +0100

    adding the search_algos.h file

---
+++ search_algorithms/README.md
@@ -0,0 +1,1 @@
+learning search algorithm
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#define _SEARCH_ALGOS_H_
+#ifndef _SEARCH_ALGOS_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
